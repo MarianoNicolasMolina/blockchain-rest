38352dad8216799a66f63718533bfefac45c644e
mariano-per <marianoo.molina@gmail.com>
2021-06-22 22:59:57 -0300

    Desarrollo de clases de estructura y transacciones junto con sus tests.

--- src/main/java/com/grupo2/blockchain/structure/Hasheable.java
+++ src/main/java/com/grupo2/blockchain/structure/Hasheable.java
@@ -1,2 +1,7 @@
-package com.grupo2.blockchain.structure;public interface Hasheable {
+package com.grupo2.blockchain.structure;
+
+public interface Hasheable {
+
+    public String getHash();
+    public String recalculateHash();
 }
--- src/main/java/com/grupo2/blockchain/structure/MerkleBlock.java
+++ src/main/java/com/grupo2/blockchain/structure/MerkleBlock.java
@@ -1,2 +1,34 @@
-package com.grupo2.blockchain.structure;public class MerkleBlock {
+package com.grupo2.blockchain.structure;
+
+import java.util.List;
+
+public class MerkleBlock<T extends Hasheable> implements Hasheable {
+
+    private String prevHash;
+    private MerkleTree<T> merkleTree;
+
+    public MerkleBlock() {}
+
+    public MerkleBlock(String prevHash, List<T> data){
+        this.prevHash = prevHash;
+        this.merkleTree = new MerkleTree<T>(data);
+    }
+
+    @Override
+    public String getHash() {
+        return merkleTree.getHash();
+    }
+
+    @Override
+    public String recalculateHash() {
+        return merkleTree.recalculateHash();
+    }
+
+    public String getPrevHash() {
+        return prevHash;
+    }
+
+    public MerkleTree<T> getMerkleTree() {
+        return merkleTree;
+    }
 }
